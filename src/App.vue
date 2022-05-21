134aca8344bc98737110eef02d97bebc3d50e8b3
lishu <lilili925@icloud.com>
2022-05-22 00:14:03 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<Monster />
+<div>Monster</div>
+</template>
+
+<script>
+import Monster from './components/Monster.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Monster:Monster,
+  }
+}
+</script>
---
+++ src/components/Monster.vue
@@ -0,0 +1,18 @@
+<template>
+<div>hello</div>
+<div>hello</div>
+<div>hello</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Monster',
+  props: {
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
